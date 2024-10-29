23eb54eed6af170aa744080d44ea6a91b823d3c3
Lucas Opoka <lucasopoka@icloud.com>
2024-10-29 14:03:36 +0200

    Working version of ServersManager

--- source/ServersManager.cpp
+++ source/ServersManager.cpp
@@ -6,10 +6,23 @@
 /*   By: lopoka <lopoka@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 13:51:39 by lopoka            #+#    #+#             */
-/*   Updated: 2024/10/28 19:49:15 by lopoka           ###   ########.fr       */
+/*   Updated: 2024/10/29 14:02:19 by lopoka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <ServersManager.hpp>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <stdlib.h>
+#include <sstream>
+#include <sys/epoll.h>
+#include <signal.h>
+
+
+
 
 ServersManager::~ServersManager()
 {
@@ -35,6 +48,16 @@ void ServersManager::addSocket(std::string &port, Server *server)
 	std::cout << "Servers in socket: " << _portsToSockets[port]->getServers().size() << std::endl;
 }
 
+bool ServersManager::_setNonblocking(int socketFd)
+{
+	int flags = fcntl(socketFd, F_GETFL, 0);
+	if (fcntl(socketFd, F_SETFL, flags | O_NONBLOCK) == -1)
+	{
+		perror("fcntl");
+		return false;
+	}
+	return true;
+}
 
 void ServersManager::_initSockets()
 {
@@ -49,12 +72,15 @@ void ServersManager::_initSockets()
 			perror("socket");
 			//return false; We could throw errors
 		}
+		
+		_setNonblocking(socketFd);
+
 		if (setsockopt(socketFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
 		{
 			perror("setsockopt");
 			//return false;
 		}
-
+		
 		std::string str = itr->first;
 		size_t delim_pos = str.find(":");
 		std::string ip = str.substr(0, delim_pos);
@@ -68,31 +94,29 @@ void ServersManager::_initSockets()
 
 		if (bind(socketFd, (sockaddr *)&socket_addr, sizeof(socket_addr)) == -1)
 		{
-			perror("bind");
+			perror("bind AAAAA");
 			//return false;
 		}
+		if (::listen(socketFd, LISTEN_BACKLOG) == -1)
+		{
+			perror("listen");
+			//return false;
+		}
+		
 		itr->second->setSocketDescriptor(socketFd);
 		_socketFdToSockets.insert({socketFd, itr->second});
 	}
 }
 
-
-void ServersManager::listen()
+bool ServersManager::listen()
 {
 	struct epoll_event events[MAX_EVENTS];
-
-	/*if (::listen(this->_socket_fd, LISTEN_BACKLOG) == -1)
-	{
-		perror("listen");
-		return false;
-	}
-	std::cout << "[webserv] server listening on port: " << this->_port << std::endl;*/
 
 	_epoll_fd = epoll_create1(0);
 	if (_epoll_fd == -1)
 	{
 		perror("epoll_create");
-		//return false;
+		return false;
 	}
 
 	for (std::map<int, Socket*>::iterator itr = _socketFdToSockets.begin(); itr != _socketFdToSockets.end(); itr++)
@@ -104,7 +128,7 @@ void ServersManager::listen()
 		if (::epoll_ctl(_epoll_fd, EPOLL_CTL_ADD, itr->first, &ev) == -2) // itr->first == socketFd
 		{
 			perror("epoll_ctl");
-			//return false;
+			return false;
 		}
 	}
 	
@@ -116,15 +140,107 @@ void ServersManager::listen()
 			break;
 		for (int i = 0; i < nfds; i++)
 		{
+			bool registered = false;
 			epoll_event &event = events[i];
 			for (std::map<int, Socket*>::iterator itr = _socketFdToSockets.begin(); itr != _socketFdToSockets.end(); itr++)
 			{
 				if (event.data.fd == itr->first) //Queue requests
-					itr->second->getServers()[0]->accept_client(); // for now the first server in the sockt accepts, later on we can find correct host
-				else if ((event.events & EPOLLIN) || (event.events & EPOLLOUT))
-					itr->second->getServers()[0]->handle_event(event);
+				{
+					registered = true;
+					_socketAcceptClient(itr->first);
+				}
 			}
+			if (registered)
+				continue;
+			if ((event.events & EPOLLIN) || (event.events & EPOLLOUT))
+					_handleEvent(event);
 		}
 	}
-	//return true;
+	return true;
 }
+
+bool ServersManager::_socketAcceptClient(int socketFd)
+{
+	struct sockaddr_in peer_addr;
+	socklen_t peer_addr_size = sizeof(peer_addr);
+	struct epoll_event ev;
+
+	int client_fd = accept(socketFd, (sockaddr *)&peer_addr, &peer_addr_size);
+	if (client_fd == -1)
+	{
+		std::cerr << "accept() failed" << std::endl;
+		return false;
+	}
+	_setNonblocking(client_fd);
+	ev.events = EPOLLIN | EPOLLET;
+	ev.data.fd = 0;
+	ev.data.ptr = new Client(client_fd, inet_ntoa(peer_addr.sin_addr));
+	if (epoll_ctl(_epoll_fd, EPOLL_CTL_ADD, client_fd, &ev) == -1)
+	{
+		std::cerr << "epoll_ctl accept failed" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool ServersManager::_handleEvent(epoll_event &event)
+{
+	Client *client = (Client *)event.data.ptr;
+	struct epoll_event ev_new;
+
+	if (client == NULL)
+		return false;
+	if (event.events & EPOLLIN) //read
+	{
+		char buffer[2048 + 1] = { 0 };
+		ssize_t bytes_read = read(client->fd, buffer, 2048);
+		if (bytes_read < 0)
+		{
+			perror("read");
+			return false;
+		}
+		std::cout << "[webserv] request received " << client->ip_addr
+			  << std::endl;
+		Request req;
+		std::string req_str(buffer, bytes_read);
+		req.parse(req_str);
+		req.dump();
+	
+		//std::cout << buffer << std::endl;
+		ev_new.events = EPOLLOUT | EPOLLET;
+		ev_new.data.fd = 0;
+		ev_new.data.ptr = event.data.ptr;
+		if (epoll_ctl(this->_epoll_fd, EPOLL_CTL_MOD, client->fd, &ev_new) == -1)
+		{
+			perror("epoll_ctl");
+			return false;
+		}
+	} else if (event.events & EPOLLOUT) //write
+	{
+		std::cout << "[webserv] write " << client->ip_addr << std::endl;
+		_response(client->fd);
+		if (epoll_ctl(this->_epoll_fd, EPOLL_CTL_DEL, client->fd, NULL) == -1)
+		{
+			perror("epoll_ctl");
+			return false;
+		}
+
+		close(client->fd);
+		delete client;
+	}
+	return true;
+}
+
+void ServersManager::_response(int client_fd)
+{
+	std::string html =
+		"<!DOCTYPE html><html lang=\"en\"><body><h1>[WEBSERVER]</h1><p>HELLO WORLD</p></body></html>";
+	std::ostringstream ss;
+	ss << "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: " << html.size()
+	   << "\n\n"
+	   << html;
+	std::string html_response = ss.str();
+
+	write(client_fd, html_response.c_str(), html_response.size());
+}
+
--- source/main.cpp
+++ source/main.cpp
@@ -5,13 +5,40 @@
 
 int main(void)
 {
-	std::cout << "[webserv]" << std::endl;
+	/*std::cout << "[webserv]" << std::endl;
 	Server *server = new Server();
 	server->add("127.0.0.1", 7051);
 	server->add("127.0.0.1", 7052);
 	server->add("127.0.0.1", 7053);
 	server->epoll();
 	server->close_server();
-	delete server;
+	delete server;*/
+
+	ServersManager sm;
+
+	Server *server1 = new Server();
+	//server1->add("127.0.0.1", 7051);
+	
+	Server *server2 = new Server();
+	//server2->add("127.0.0.1", 7052);
+	
+	Server *server3 = new Server();
+	//server3->add("127.0.0.1", 7053);
+	
+	Server *server4 = new Server();
+	//server4->add("127.0.0.1", 7054);
+
+	std::string port1("127.0.0.1:7051");
+	sm.addSocket(port1, server1);
+	std::string port2("127.0.0.1:7052");
+	sm.addSocket(port2, server2);
+	std::string port3("127.0.0.1:7053");
+	sm.addSocket(port3, server3);
+	std::string port4("127.0.0.1:7054");
+	sm.addSocket(port4, server4);
+
+	sm._initSockets();
+	sm.listen();
+
 	return 0;
 }
